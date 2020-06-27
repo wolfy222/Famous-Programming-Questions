fff0ee61eca9b96b5fc64de865b783343133a017
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-06-27 15:23:51 +0530

    Stack Ques

--- Compititve Programming/src/listInterface/ParenthesisChecker.java
+++ Compititve Programming/src/listInterface/ParenthesisChecker.java
@@ -16,7 +16,9 @@ Declare a character stack S.
 Now traverse the expression string exp.
 If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
 If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped character is the matching starting bracket then fine else parenthesis are not balanced.
-After complete traversal, if there is some starting bracket left in stack then “not balanced”
+After complete traversal, if there is some starting bracket left in stack then “not balanced”\
+
+
  */
 package listInterface;
 
